30f2b824b95f3be5a73f78948b0c5f59af0e3d9d
linguini1 <matteo.golin@gmail.com>
2020-11-27 10:40:08 -0500

    Added more thorough comments and changed the encryption so that messages are hexed before being encoded via RSA.

--- coderRSA.py
+++ coderRSA.py
@@ -1,13 +1,23 @@
 # RSA Encryption Method
 
+# Imports
+import binascii
 
-# Multiply & Square algorithm
+# Default file name
+name = "RSA_file.txt"
+
+
+# Multiply & Square algorithm allowing for much faster encryption and decryption
 
 def multiply_square_e(base, power, modulo):
     result = 1
     for binary in str(format(power, 'b')):
+
+        # If the bit is zero, then it just gets set to the power of two, mod modulo
         if binary == "0":
             result = (result ** 2) % modulo
+
+        # If the bit is one, then it gets multiplied by the base as well
         elif binary == "1":
             result = ((result ** 2) * base) % modulo
     return result
@@ -25,55 +35,75 @@ def multiply_square_d(base, power, modulo):
 
 
 if __name__ == '__main__':
-    # Read from file or small message
+
+    # Asks user if they want to encrypt their data or decrypt their data
     while True:
         encrypt = input("Do you want to encrypt or decrypt? (Type the word): ")
-        if encrypt == "encrypt":
+        if encrypt.lower() == "encrypt":
             encrypt = True
             break
-        elif encrypt == "decrypt":
+        elif encrypt.lower() == "decrypt":
             encrypt = False
             break
         else:
             print("Invalid input.")
+
+    # Asks user if they want to use data from a text file or just type within the console
     while True:
-        file_or_no = input("Do you want to read from a file (1) or write a small message in the console (2)? Type: ")
+        file_or_no = input("Do you want to read from a text file (1) or write a small message in the console (2)? Type: ")
+
+        # Reading from a text file
         if file_or_no == "1":
+
+            # Gets a valid directory
             while True:
                 directory = input("Which file do you want to use? Include the whole directory: ")
+
+                # If the path is contained in quotation marks, they're removed so that the program works properly
                 if '"' in directory:
                     directory = directory[1:-1]
                 try:
                     message = open(directory, 'r')
+                    # Gets the filename from the user
                     name = input("What filename do you want to save your new file under? Type: ")
-                    name = name + ".txt"
+                    name = name + ".txt"  # Adds the necessary .txt suffix
                     break
+
+                # If the path is invalid the user is re-prompted
                 except FileNotFoundError:
                     print("Invalid directory or file name, try again.")
             break
+
+        # Reading from the console
         elif file_or_no == "2":
             # Getting user string to be converted
-            message = input("Message to be encrypted: ")
+            message = input("Message to be encrypted or decrypted: ")
             break
+
+        # If the user selects an option other than 1 or 2
         else:
             print("Invalid input.")
-    print()
+    print()  # Print a line break
 
     # Getting lock
     while True:
         lock_1 = input("Lock or key in format (x, y): ")
         try:
-            lock = lock_1[1:-1].split(",")
-            e = int(lock[0])
-            n = int(lock[1])
+            lock = lock_1[1:-1].split(",")  # Removes the brackets and comma
+            e = int(lock[0])  # Sets the x value as an integer for e
+            n = int(lock[1])  # Sets the y value as an integer for n
             break
+
+        # If the lock format is incorrect
         except ValueError:
             print("Invalid lock.")
 
     # Creating dictionary
     dictionary = {}
-    characters = """QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()\x00\x1a_\xad+-=[]\{}\n|;:'<,>.?\t/`~ \""""
+    characters = """abcdef1234567890"""
     characters = list(characters)
+
+    # Gives each character its own index to be used for mathematical operations
     for index, character in enumerate(characters):
         dictionary[character] = index
 
@@ -81,14 +111,25 @@ if __name__ == '__main__':
     from multiprocessing import Pool
     from itertools import repeat
 
+    # If the user selected encryption:
     if encrypt:
-        # Switching char to num, performing math and then converting back to char
+
+        # If reading from a text file
         if file_or_no == "1":
+
+            # Switching char to num, performing math
             with open(name, 'w') as new_file:
-                for word in message:
-                    char_nums = [dictionary[character] for character in word]
-                    with Pool() as pool:
+
+                # Making a list of the indexes
+                for line in message:
+                    char_nums = [dictionary[character] for character in line.encode("utf-8").hex()]
+
+                    with Pool() as pool:  # Opening multithreading
+
+                        # Passes in the indexes, e & n values to the multiply and square algorithm as iterables
                         new_nums = pool.starmap(multiply_square_e, zip(char_nums, repeat(e), repeat(n)))
+
+                        # Saving the encrypted number to the file
                         for new_num in new_nums:
                             new_file.write(str(new_num) + " ")
 
@@ -96,11 +137,16 @@ if __name__ == '__main__':
             print(f"Your encrypted message has been saved to a file named '{name}'.")
             print(f"It has been locked with {lock_1}.")
 
+        # If reading from console
         elif file_or_no == "2":
-            new_message = ""
-            char_numbers = [dictionary[character] for character in message]
-            with Pool() as pool:
+            new_message = ""  # Empty string variable to save the encrypted message
+            char_numbers = [dictionary[character] for character in message.encode("utf-8").hex()]  # Getting list of indexes
+            with Pool() as pool:  # Opening multithreading
+
+                # Passes in the indexes, e & n values to the multiply and square algorithm as iterables
                 new_char_numbers = pool.starmap(multiply_square_e, zip(char_numbers, repeat(e), repeat(n)))
+
+                # Saving the encrypted message to the empty string variable
                 for new_char_number in new_char_numbers:
                     new_message += str(new_char_number) + " "
 
@@ -110,26 +156,42 @@ if __name__ == '__main__':
             print()
             print(new_message)
 
+    # If the user selected decryption
     elif not encrypt:
+
+        # From a text file
         if file_or_no == "1":
             with open(name, 'w') as file:
                 for row in message:
-                    line = row.split(" ")[0:-1]
-                    with Pool() as pool:
+                    line = row.split(" ")[0:-1]  # Removes the spaces between numbers
+                    with Pool() as pool:  # Opening multithreading
+
+                        # Passes in the line of numbers, e & n values as iterables to the multiply and square algorithm
                         new_nums = pool.starmap(multiply_square_d, zip(line, repeat(e), repeat(n)))
+
+                        # Decrypted index is matched to the dictionary character and then written into the hex list
+                        hexed_line = ""  # Empty string for carrying a hexed line
                         for new_num in new_nums:
                             for label, value in dictionary.items():
                                 if value == new_num:
-                                    file.write(label)
+                                    hexed_line += label
+
+                        # Write to file
+                        file.write(str(bytes.fromhex(hexed_line).decode("ASCII")))
 
             # Returning info
             print(f"Your message has been decoded and saved under filename '{name}'.")
             print()
 
+        # From the console
         elif file_or_no == "2":
-            d_message = ""
-            with Pool() as pool:
-                new_nums = pool.starmap(multiply_square_d, zip(message, repeat(e), repeat(n)))
+            d_message = ""  # Empty string variable to hold the decryption
+            with Pool() as pool:  # Getting multithreading power
+
+                # Passing in the message, the e & n values as iterables to the multiply and square algorithm
+                new_nums = pool.starmap(multiply_square_d, zip(message.split(" "), repeat(e), repeat(n)))
+
+                # Matches decrypted index to the dictionary character and then adds it to the empty string
                 for new_num in new_nums:
                     for label, value in dictionary.items():
                         if value == new_num:
@@ -138,4 +200,4 @@ if __name__ == '__main__':
             # Returning info
             print("Your decoded message is as follows:")
             print()
-            print(d_message)
+            print(str(bytes.fromhex(d_message))[2:-1])  # Convert back from hex first
--- dataRSA.py
+++ dataRSA.py
@@ -1,5 +1,7 @@
 # RSA Encryption software to just generate lock and key
-from math import gcd as bltin_gcd
+
+# Imports
+from math import gcd as bltin_gcd  # For finding the co-primes
 
 # Getting two valid primes
 while True:
@@ -8,38 +10,45 @@ while True:
     try:
         p = int(p)
         q = int(q)
+
+        # The factor of the primes must be at least 96
+        # Neither of the primes can be 1
+        # The primes can't form a perfect square
         if p * q < 96 or p == 1 or q == 1 or p == q:
             print("The product of both primes must be larger than 96, they can't be equal, and no one can equal 1.")
         elif p * q > 96:
             break
+
+    # Catches non-integer inputs
     except ValueError:
         print("Invalid input. Primes must be integers.")
 
 
 # Important numbers
-n = p * q
-coprimes = (p-1)*(q-1)
+n = p * q  # The actual product of the primes
+coprimes = (p-1)*(q-1)  # The number of coprimes
 
 
-# Functions for finding coprimes of p and q
-def coprime(num, n_or_num_of_coprimes):
+# Function for finding co-prime of a number
+def co_prime(num, n_or_num_of_coprimes):
     return bltin_gcd(num, n_or_num_of_coprimes) == 1
 
 
+# Finds a number which is both co-prime with n and the number of coprimes
 def finding_e():
     for num in range(2, n):
-        if coprime(num, n) and coprime(num, coprimes):
+        if co_prime(num, n) and co_prime(num, coprimes):
             return num
 
 
-# Getting coprimes to find e
+# Getting co-primes to find e
 e = finding_e()
 
 # Making lock
 lock = (e, n)
 
 
-# Finding decryption key fxn
+# Finding decryption key using the formula for RSA
 def find_d():
     for k in range(e):
         if (1 + k * coprimes) % e == 0:
